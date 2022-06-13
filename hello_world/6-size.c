9e0b40af0c7b5aa8693da3b968266f918f423718
paosua86 <paolasuarezb1986@gmail.com>
2022-06-13 17:51:15 -0500

    task

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+/**
+ * main - Prints the size of different variables
+ * Description: Prints the sizef different variables
+ * Return: zero meaning everything went well
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int);
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld bytes(s)\n", sizeof(float));
+	return (0);
+}
