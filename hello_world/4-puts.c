8f1d0ed67decb8db1637e131c2a975f639fd53e5
paosua86 <paolasuarezb1986@gmail.com>
2022-06-13 16:58:29 -0500

    task

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-/* print Hello World */
+/* main - Prints a specific text */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
