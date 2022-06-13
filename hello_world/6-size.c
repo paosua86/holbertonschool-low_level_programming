4b7b3641f90f102442a0b69ea420a22054f84b20
paosua86 <paolasuarezb1986@gmail.com>
2022-06-13 18:01:23 -0500

    task

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,15 +1,16 @@
-#include <stdio.h> 
+#include <stdio.h>
 /**
  * main - Prints the size of different variables
- * Description: Prints the sizef different variables
+ * Description: Prints the sizof different variables
+ * as seen in the list
  * Return: zero meaning everything went well
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int);
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
