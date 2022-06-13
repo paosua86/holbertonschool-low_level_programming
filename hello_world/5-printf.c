368374a2261a7da5b9ca4dda515070284d15619d
paosua86 <paolasuarezb1986@gmail.com>
2022-06-13 16:47:24 -0500

    task

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* print phrase with printf */
+/**
+ * * main - Print a text
+ * * Description: This function prints another text
+ * * Return: zero, meaning everything went well.
+ * */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
