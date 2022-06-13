e43dfb5005cf7dc6d34f906f0ee5e30359949ffd
paosua86 <paolasuarezb1986@gmail.com>
2022-06-13 17:00:47 -0500

    task

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
-/* main - Prints a specific text */
+/**
+ * main - Prints a specific text
+ * Description: This function prints the text
+ * Return: zero to confirm everything went well
+ */ 
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
