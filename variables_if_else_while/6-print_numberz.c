30c5ea140be4d028b28b085ed039681f9d85087b
paosua86 <paolasuarezb1986@gmail.com>
2022-06-14 15:57:48 -0500

    task

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -5,8 +5,8 @@
  * main - Prints base 10 numbers
  * Description prints numbers 0-9
  * Return: 0 for success
- * Enfoque: La idea es utilizar el valor 
- * ASCII de los dígitos del 
+ * Enfoque: La idea es utilizar el valor
+ * ASCII de los dígitos del
  * 0 al 9 comenzando desde 48 - 57
  */
 int main(void)
