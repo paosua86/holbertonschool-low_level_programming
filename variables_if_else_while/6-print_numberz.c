51378d3f63f97c78250440d00aafa95b29c31407
paosua86 <paolasuarezb1986@gmail.com>
2022-06-14 15:54:36 -0500

    task

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Prints base 10 numbers
+ * Description prints numbers 0-9
+ * Return: 0 for success
+ * Enfoque: La idea es utilizar el valor ASCII de los dígitos del 0 al 9 comenzando desde 48 - 57
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
