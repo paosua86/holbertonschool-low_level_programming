9dc6c56c48ef1b2c7bd3131c1ec1c4b6c7ffdb51
paosua86 <paolasuarezb1986@gmail.com>
2022-06-15 15:30:12 -0500

    task

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - check a letter
+ * @c: letter u other
+ * Return: 1 if is letter 0 others
+ */
+
+int _isalpha(int c)
+
+{
+
+	if (c > 65 && c < 90 || c > 97 && < 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
