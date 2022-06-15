fa36e2367ecb1e6e286e09d42531e1c34c553189
paosua86 <paolasuarezb1986@gmail.com>
2022-06-15 15:13:18 -0500

    task

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+ /**
+	* _islower - check lowercase character
+	* Description: print alphabet 10 times
+	* Return: zero
+*/
+
+int _islower(int c)
+
+{
+
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
