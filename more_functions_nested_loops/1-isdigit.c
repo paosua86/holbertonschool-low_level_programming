c1ad063486efa9d7f0dd32d90485b33fe3d061f5
paosua86 <paolasuarezb1986@gmail.com>
2022-06-20 10:31:34 -0500

    task

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase character
+ * Return: 1 uppercase and 0 otherwise
+ * @c: character to be checked
+ */
+
+int _isdigit(int c);
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
