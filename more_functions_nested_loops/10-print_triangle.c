a3089c2a93e8edc59131e34371d7c115e6dab5f1
paosua86 <paolasuarezb1986@gmail.com>
2022-06-20 15:33:02 -0500

    task

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,11 +10,11 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 1; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
 			for (j = size - i; j > 0; j--)
 				_putchar(32);
-			for (j = i; j >= 0; j--)
+			for (j = i; j > 0; j--)
 				_putchar(35);
 			_putchar(10);
 		}
