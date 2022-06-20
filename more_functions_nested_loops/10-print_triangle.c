b616b18dbd29dab17ffe614defbcb1b9f0fcae18
paosua86 <paolasuarezb1986@gmail.com>
2022-06-20 15:30:07 -0500

    task

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *  print_triangle - prints a triangle
+ *  @size: number of times
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i < size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+				_putchar(32);
+			for (j = i; j > 0; j--)
+				_putchar(35);
+			_putchar(10);
+		}
+	}
+	else
+	{
+		_putchar(10);
+
+	}
+}
