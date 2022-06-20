c518b3c05a68c1fd05d6e1eaae58f641d4d4b689
paosua86 <paolasuarezb1986@gmail.com>
2022-06-20 11:04:36 -0500

    task

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * Return:  (a * b)
+ * @a: int
+ * @b: int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints the numbers
+ * from 0 to 9, followed by a new lin
+ * Return: character
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
