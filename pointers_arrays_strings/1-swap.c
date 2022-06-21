cf33c4539215a58358c364194723977ab03f830d
paosua86 <paolasuarezb1986@gmail.com>
2022-06-21 10:53:11 -0500

    task

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: pointer that will update the value of s
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
