6df22db4373f68d91a3aefc5de91cc760bcd05c5
paosua86 <paolasuarezb1986@gmail.com>
2022-06-22 15:17:53 -0500

    task

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,11 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; dest[i] != '\0'; i++)
 
-	for (j = 0; dest[j] != '\0'; j++)
+	for (j = 0; src[j] != '\0'; j++, i++)
 	{
 		dest[i] = src[j];
-		i++;
 	}
-	dest[i] = src[j];
 	return (dest);
 }
