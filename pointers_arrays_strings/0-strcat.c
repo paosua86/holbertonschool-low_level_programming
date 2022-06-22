28a689fc78f1d8395bc9a46048face80b6cbeb35
paosua86 <paolasuarezb1986@gmail.com>
2022-06-22 15:21:01 -0500

    task

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,21 @@
 #include "main.h"
-
 /**
- * _strcat - function that concatenates two strings
- * @dest: pointer to destination char
- * @src: pointer to source char
- * Return: char
+ * _strcat - concatenates the string from a pointer to another
+ * @src: pointer that has the string to be concatenated
+ * @dest: pointer that will have its string concatenated with the other
+ * Return: strings concatenated to dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+		int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-
-	for (j = 0; src[j] != '\0'; j++, i++)
-	{
-		dest[i] = src[j];
-	}
-	return (dest);
+		for (i = 0; dest[i] != 0; i++)
+		{
+		}
+		for (j = 0; src[j] != 0; j++, i++)
+		{
+			dest[i] = src[j];
+		}
+		return (dest);
 }
