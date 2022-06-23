59dfe3b7f56489ff382be52ccb0a4f081e7aeab5
paosua86 <paolasuarezb1986@gmail.com>
2022-06-23 12:11:03 -0500

    task

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,15 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-		int i, j;
+		int i;
 
-		for (i = 0; dest[i] != 0; i++)
+		for (i = 0; i < n && src[i] != 0; i++)
 		{
+			dest[i] = src[i];
 		}
-		for (j = 0; j < n && src[j] != 0; j++, i++)
+		for (; i < n; i++)
 		{
-			dest[i] = src[j];
+			dest[i] = 0;
 		}
 		return (dest);
 }
