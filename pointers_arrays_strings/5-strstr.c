c6cd630ee7bd43ec1964e25c300970c49738fa31
paosua86 <paolasuarezb1986@gmail.com>
2022-06-24 15:37:58 -0500

    task

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - Write a function that locates a substring
+ * @haystack: pointer
+ * @needle: pointer
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (needle[0] == '\0')
+		return (haystack);
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+			if (haystack[i + j] == '\0')
+				return (haystack + i);
+		}
+	}
+	return (0);
+}
