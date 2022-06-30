5a506968c8e4334d568300616a7c2cb33ea7815b
paosua86 <paolasuarezb1986@gmail.com>
2022-06-30 12:51:24 -0500

    task

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+	* _print_rev_recursion - prints string in reverse
+	*	@s: pointer
+	*	Return: zero
+	*/
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		return;
+}
