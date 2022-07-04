ccc18a18a391fd539fcaa324d2d933fc973b9e2d
paosua86 <paolasuarezb1986@gmail.com>
2022-07-04 16:37:33 -0500

    task

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program that calls its name
+ * @argc - argument count
+ * @argv - vector
+ * Return: 0
+ */
+
+
+int main (int argc, char *argv[])
+{
+	int = 1;
+
+	if (argv > 1)
+	{
+		printf("argv[%d] = %s\n", i, argv[i]);
+	}
+	return 0;
+}
+
