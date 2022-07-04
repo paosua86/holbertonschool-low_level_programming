f35ebc556baf1634863a642322b144664f7543ab
paosua86 <paolasuarezb1986@gmail.com>
2022-07-04 17:49:36 -0500

    task

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -11,14 +11,14 @@
 
 int main(int argc, char *argv[])
 {
-	int i; 
+	int i;
 
 	if (argc > 0)
 	{
-		for(i = 0; argc > i; i++)
+		for (i = 0; argc > i; i++)
 		{
 			printf("%s\n", argv[i]);
-		}	
+		}
 	}
 	return (0);
 }
