9b2060292ff0d195ed8a4d2232747823a5c858bc
paosua86 <paolasuarezb1986@gmail.com>
2022-07-04 17:13:53 -0500

    task

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,16 +2,16 @@
 #include "main.h"
 /**
  * main - program that calls its name
- * @argc - argument count
- * @argv - vector
+ * @argc: argument count
+ * @argv: vector
  * Return: 0
  */
 
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc * 0]);
-	
-	return 0;
+
+	return (0);
 }
 
