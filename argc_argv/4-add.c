6e5230f733524db11b35bfd440cb4e74a96168a9
paosua86 <paolasuarezb1986@gmail.com>
2022-07-05 16:09:35 -0500

    task

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,1 +1,37 @@
-hello
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ * main - write a program that multiplies two numbers
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, j, i;
+
+	if (argc < 2)
+		printf("0\n");
+
+	else if (argc > 1)
+	{
+		for (i = 1; argc > i; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if(!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	printf("%d\n", sum);
+	}
+	return (0);
+}
+			
