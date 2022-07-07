55f515ff1077576453c2fa7f2d8c38d9c3ba470e
paosua86 <paolasuarezb1986@gmail.com>
2022-07-06 19:24:19 -0500

    task

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,16 +15,20 @@ char *create_array(unsigned int size, char c)
 	char *a;
 	unsigned int i;
 
-	if (size == '0')
-		return (NULL);
-
-	a = malloc(size * sizeof(char));
+	if (size == 0)
+	{
+		return (0);
+	}
+	a = malloc(sizeof(char) * size);
 
 	if (a == 0)
+	{
 		return (0);
-
+	}
 	for (i = 0; i < size; i++)
+	{
 		*(a + i) = c;
+	}
 
 	return (a);
 }
