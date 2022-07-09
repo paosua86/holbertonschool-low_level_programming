caa331a70ac6864bfb0bb0e17c9192a415ea2d7d
paosua86 <paolasuarezb1986@gmail.com>
2022-07-09 18:51:33 -0500

    task

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
-		printf("Name: nil\n");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 
 
 	if (d->owner == NULL)
-		printf("Owner: nil\n");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
