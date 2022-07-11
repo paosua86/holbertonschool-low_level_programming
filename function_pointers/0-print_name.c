9a18fb7a7bede3707bd177cd7eb77fce3e3fede5
paosua86 <paolasuarezb1986@gmail.com>
2022-07-11 12:32:08 -0500

    task

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+
+/**
+ * print_name - Write a function that prints a name
+ * @name: char
+ * @f: function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
