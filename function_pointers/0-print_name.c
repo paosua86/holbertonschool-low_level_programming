36f0cd5839b7d6b9b2561b0df7301a82ed7451f3
paosua86 <paolasuarezb1986@gmail.com>
2022-07-11 12:37:28 -0500

    task

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,8 +1,6 @@
 #include "function_pointers.h"
-
-
 /**
- * print_name - Write a function that prints a name
+ * print_name - Write a function that prints a name.
  * @name: char
  * @f: function
  * Return: nothing
