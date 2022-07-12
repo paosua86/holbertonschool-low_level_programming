a15c8260efcc9b6c1c5f5e8cf95de3fa0ba7943f
paosua86 <paolasuarezb1986@gmail.com>
2022-07-12 18:07:32 -0500

    task

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,21 +11,24 @@
 
 int *array_range(int min, int max)
 {
-	int *array;
-	int i;
+	int *array = NULL;
+	int i, count;
 
 	if (min > max)
 		return (NULL);
 
-	array = malloc(sizeof(array) * ((max - min) + 1));
+	count = (max - min) + 1;
+
+	array = malloc(sizeof(int) * count);
 
 	if (array == NULL)
 	{
 		free(array);
 		return (NULL);
 	}
-	for (i = 0; min <= max; i++, min++)
+	for (i = 0; i < count; i++, min++)
+	{	
 		array[i] = min;
-
+	}
 	return (array);
 }
