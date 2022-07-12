2df188899b9e6ac393db9d916e13580704692458
paosua86 <paolasuarezb1986@gmail.com>
2022-07-12 16:08:33 -0500

    task

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,10 +18,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *nconcat;
 
 	if (s1 == NULL)
-		return (NULL);
+		return ("");
 
 	if (s2 == NULL)
-		return (NULL);
+		return ("");
 
 	for (i = 0; s1[i]; i++)
 	{}
