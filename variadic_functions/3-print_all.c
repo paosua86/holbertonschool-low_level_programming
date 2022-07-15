566103f4f66c946d70f3460215a730d87ce4bcbb
paosua86 <paolasuarezb1986@gmail.com>
2022-07-15 14:51:36 -0500

    task

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,23 +1,20 @@
 #include "variadic_functions.h"
-
 /**
  * print_all - Write a function that prints anything
  * @format: is a list of types of arguments passed to the function
  * Return: prints anything
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list p_all;
-	int i = 0, j; 
-	char *str;
-	char letter[] = {'c', 'i', 'f', 's'};
+	int i = 0, j;
+	char letter[] = {'c', 'i', 'f', 's'}, *str;
 
 	va_start(p_all, format);
 	while (format != NULL && format[i])
 	{
 		switch (format[i])
-		{	
+		{
 			case 'c':
 				printf("%c", va_arg(p_all, int));
 				break;
@@ -28,7 +25,7 @@ void print_all(const char * const format, ...)
 				printf("%f", va_arg(p_all, double));
 				break;
 			case 's':
-				str = (va_arg(p_all, char*));
+				str = va_arg(p_all, char*);
 				if (!str)
 				{
 					printf("(nil)");
@@ -39,7 +36,7 @@ void print_all(const char * const format, ...)
 		}
 		j = 0;
 		while (j < 4)
-		{	
+		{
 			if ((letter[j] == format[i]) && format[i + 1])
 			{
 				printf(", ");
@@ -48,6 +45,5 @@ void print_all(const char * const format, ...)
 		}
 		i++;
 	}
-	printf("\n");
-	va_end(p_all);
+	printf("\n"), va_end(p_all);
 }
