f2d02c6a9940d6bb703790ab3137f32f151701b4
paosua86 <paolasuarezb1986@gmail.com>
2022-07-15 08:22:47 -0500

    task

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,25 +2,25 @@
 
 /**
  * sum_them_all - Write a function that returns the sum of all its parameters
- * @n: const unsigned int
- * Return: depends
+ * @n: is the number of integers passed to the function
+ * Return: sum
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list numbers;
+	va_list valist;
 	unsigned int i;
-	int sum;
-
+	int sum = 0;
 
 	if (n == 0)
-		return 0;
-
-	va_start(numbers, n);
+	{
+		return (0);
+	}
+	va_start(valist, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(numbers, int);
-
-	va_end(numbers);
-
+	{
+		sum += va_arg(valist, int);
+	}
+	va_end(valist);
 	return (sum);
 }
