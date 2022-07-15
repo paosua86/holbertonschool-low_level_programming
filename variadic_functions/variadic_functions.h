9a348f113d6b9b29e14bd03c519eab042140e6a2
paosua86 <paolasuarezb1986@gmail.com>
2022-07-15 08:21:25 -0500

    task

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+
+#endif /* #ifndef HEADER_FILE */
