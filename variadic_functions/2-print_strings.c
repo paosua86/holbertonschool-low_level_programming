111f88995f52da4f582c19a44c075032311da5ca
paosua86 <paolasuarezb1986@gmail.com>
2022-07-15 08:41:06 -0500

    task

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s", va_arg(strings, char*));
 		if (separator != NULL && i < (n - 1))
 			printf("%s", separator);
-		if (strings == '\0')
+		if (strings == NULL)
 			printf("nil");
 	}
 	printf("\n");
