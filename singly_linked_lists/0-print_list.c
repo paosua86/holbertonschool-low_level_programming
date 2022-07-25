800e2e7272a685854cb363a9d399104eb6e63abf
paosua86 <paolasuarezb1986@gmail.com>
2022-07-25 18:16:57 -0500

    tasasks

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/* print_list - Write a function that prints all the elements of a list_t list
+ * @list_t
+ *
+ */
+
+size_t print_list(const list_t *h)
+{
+
+	size_t count;
+
+	if (h == NULL)
+		return (0);
+
+	for (count = 0; h != NULL; count++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next;
+		}
+	}	
+	return (count);
+}	
---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,31 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ * 
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
+
+/*prototypes*/
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+
+
+#endif /* #ifndef HEADER_FILE */
