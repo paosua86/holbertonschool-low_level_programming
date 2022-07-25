4cb6b12b04835f470fe6fe4c7f421dffd6f7c013
paosua86 <paolasuarezb1986@gmail.com>
2022-07-25 18:45:20 -0500

    task

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -17,16 +17,7 @@ size_t list_len(const list_t *h)
 
 	for (count = 0; h != NULL; count++)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-			h = h->next;
-		}
-		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-		}
+		h = h->next;
 	}
 	return (count);
 }
