1a6c39b9c45438c29c8bc4adc1c8c0b5e5c8a03b
paosua86 <paolasuarezb1986@gmail.com>
2022-07-26 16:37:18 -0500

    task

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+
+__attribute__ ((constructor))
+
+/**
+ * printf
+ */
+
+void print()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+}
