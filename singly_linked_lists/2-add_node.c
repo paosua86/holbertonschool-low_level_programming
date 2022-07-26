c5e607e0774ddb1dc05eaeea8fe659f69f3960c0
paosua86 <paolasuarezb1986@gmail.com>
2022-07-26 14:09:31 -0500

    task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,21 +1,6 @@
 #include "lists.h"
 
 /**
- * _strlen - finds the length of a string
- * @str: string
- * Return: length of string
- */
-
-unsigned int _strlen(char *str)
-{
-	unsigned int count;
-
-	for (count = 0; str[count]; count++)
-		;
-	return (count);
-}
-
-/**
  * add_node - Write a function that prints all the elements of a list_t list
  * @str: string
  * @head: doble pointer to a lined list
@@ -25,24 +10,15 @@ unsigned int _strlen(char *str)
 list_t *add_node(list_t **head, const char *str)
 {
 
-	list_t *new_node;
-
-	if (str == NULL)
-		return (NULL);
+	list_t *new_node = NULL;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
-
+	
 	new_node->str = strdup(str);
-
-	new_node->len = _strlen(new_node->str);
-
+	new_node->len = strlen(str);
 	new_node->next = *head;
-
 	*head = new_node;
 
 	return (new_node);
