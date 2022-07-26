ea46e6a7a02f832c7da908365c6dc0775000a220
paosua86 <paolasuarezb1986@gmail.com>
2022-07-26 16:40:30 -0500

    task

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -4,11 +4,14 @@
 __attribute__ ((constructor))
 
 /**
- * printf
+ * print - Write a function that prints You're beat!
+ * and yet, you must allow,\nI bore my house
+ * upon my back!\n before the main function is executed
  */
 
-void print()
+void print(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+					"I bore my house upon my back!\n");
 
 }
