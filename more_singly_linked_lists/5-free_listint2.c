455984f23b7d03a9ceb26170ffb79c686ee84a2e
paosua86 <paolasuarezb1986@gmail.com>
2022-07-27 17:02:46 -0500

    task

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Write a function that frees a listint_t list
+ * @head: head
+ * Return: new node
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	tmp = *head;
+	while (tmp)
+	{
+		tmp = tmp->next;
+		free(*head);
+		*head = tmp;
+	}
+	*head = NULL;
+}
