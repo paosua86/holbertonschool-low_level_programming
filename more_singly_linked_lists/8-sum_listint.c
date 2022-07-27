69341644e67eb645c587403d942a8eadafc9c27a
paosua86 <paolasuarezb1986@gmail.com>
2022-07-27 18:54:51 -0500

    task

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * sum_listint - Write a function that returns the sum of all the data (n) of a listint_t linked list
+ * sum_listint - Write a function that returns
+ * the sum of all the data (n) of a listint_t linked list
  * @head: head pointer
  * Return: sum
  */
