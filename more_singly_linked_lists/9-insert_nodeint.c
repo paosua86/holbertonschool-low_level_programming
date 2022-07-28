37422bc995a5301502489ccf69acbd60aae21409
paosua86 <paolasuarezb1986@gmail.com>
2022-07-28 16:07:29 -0500

    task

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		free(new);
 		return (NULL);
 	}
-	if (index == 0)
+	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
