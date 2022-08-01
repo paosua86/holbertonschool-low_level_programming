845576730a7d039b18aa045f06e11e53c274015e
paosua86 <paolasuarezb1986@gmail.com>
2022-08-01 18:13:12 -0500

    task

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -10,9 +10,12 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
+	unsigned long int set;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
-	*n = ((unsigned long int)(~(0x1 << index)) & *n);
+	set = ~(1 << index);
+	*n = *n & set;
 	return (1);
 }
