bdd49327e1db8a48128285e53620a4c2c6e7addc
paosua86 <paolasuarezb1986@gmail.com>
2022-08-02 11:23:18 -0500

    task

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - Write a function
- * that reads a text file and prints it to the
- * POSIX standard output
+ * create_file - Create a function that creates a file
  * @filename: is the name of the file to create
  * @text_content: is a NULL terminated string to write to the file
  * Return: 1 or -1
@@ -15,18 +13,21 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	
+
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
-	
+
 	if (text_content)
 	{
 		for (n = 0; text_content[n]; n++)
-				write(file, text_content, n);
+		{
+			write(file, text_content, n);
+		}
 	}
 	else
 		write(file, "", 0);
+
 	close(file);
 	return (1);
 }
