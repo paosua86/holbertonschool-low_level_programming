f6e6bf05ec181b112d3f911d661f7b7c5a78db65
paosua86 <paolasuarezb1986@gmail.com>
2022-08-02 11:34:42 -0500

    task

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, n, file_write;
+	int file, file_write;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,8 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 
-	for (n = 0; text_content[n]; n++)
-	file_write = write(file, text_content, n);
+	file_write = write(file, text_content, strlen(text_content));
 	close(file);
 
 	if (file_write == -1)
