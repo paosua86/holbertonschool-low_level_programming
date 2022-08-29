ae35615e040214adc60e6d6316ef7861316fa5c2
paosua86 <paolasuarezb1986@gmail.com>
2022-08-29 17:11:46 -0500

    style changes

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -5,7 +5,7 @@
  * @key: is the key
  * @size: is the size of the array of the hash table
  * Return: index at which the key/value pair should be
- stored in the array of the hash table
+ * stored in the array of the hash table
 */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
