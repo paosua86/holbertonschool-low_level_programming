a0db394018591d2b54f487bac11b4b1021e642f5
paosua86 <paolasuarezb1986@gmail.com>
2022-08-29 17:10:15 -0500

    style changes

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -4,11 +4,11 @@
  * key_index - Write a function that gives you the index of a key
  * @key: is the key
  * @size: is the size of the array of the hash table
- * Return: index at which the key/value pair should be stored in the array of the hash table
+ * Return: index at which the key/value pair should be
+ stored in the array of the hash table
 */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-
-	
+	return (hash_djb2(key) % size);
 }
