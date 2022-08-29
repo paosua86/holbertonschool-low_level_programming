a89b808f3fe41bb3f9134f1dbb5eae788d23e00e
paosua86 <paolasuarezb1986@gmail.com>
2022-08-29 15:26:00 -0500

    pointer error in 0 and style

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,6 +1,10 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -27,7 +31,7 @@ typedef struct hash_node_s
 typedef struct hash_table_s
 {
 	unsigned long int size;
-	hash_node_t **array;
+	hash_node_t **array; 
 } hash_table_t;
 
 
