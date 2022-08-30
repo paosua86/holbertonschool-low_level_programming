736837b8eb303b14dbc417e8380213ff57658ddc
paosua86 <paolasuarezb1986@gmail.com>
2022-08-30 11:18:25 -0500

    add 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Write a function that prints a hash table.
+ * @ht: ht is the hash table
+ * Return: nothing
+*/
+
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int i;
+	hash_node_t *delete;
+
+	if (ht != NULL)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			while (ht->array[i] != NULL)
+			{
+				delete = ht->array[i]->next;
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = delete;
+			}
+		}
+		free(ht->array);
+		ht->array = NULL;
+		ht->size = 0;
+		free(ht);
+	}
+}
