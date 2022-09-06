171c4504d06354a5278b7341a3ee574c1d315ade
paosua86 <paolasuarezb1986@gmail.com>
2022-09-06 18:29:00 -0500

    no info

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,16 @@
+#include "search.algos.h"
+
+/**
+ * linear_search - writes the character c to stdout
+ * @array:  is a pointer to the first element of
+ * the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: 1 or -1
+ */
+
+
+int linear_search(int *array, size_t size, int value)
+{
+    
+}
